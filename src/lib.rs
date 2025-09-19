c7583ff8eb59fb990221e1911b7c165f071c91d0
ob2001 <butlero2001@outlook.com>
2025-09-18 23:38:52 -0400

    Create type and trait aliases for convenience

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
+#![feature(trait_alias)]
+
+use std::{rc::Rc, cell::RefCell};
+
 pub mod event_handler;
 
-pub trait Emitter<Ev: Copy> {
+pub type EmCRc<Ev> = Rc<RefCell<dyn Emitter<Ev>>>;
+pub type LiCRc<Ev> = Rc<RefCell<dyn Listener<Ev>>>;
+
+pub trait Event = PartialEq + Copy + 'static;
+
+pub trait Emitter<Ev: Event> {
     // Cause emitter to emit events without regard
     // for context.
     // Implementation specific to each emitter.
@@ -8,7 +17,7 @@ pub trait Emitter<Ev: Copy> {
     fn emit(&self) -> &Option<Vec<Ev>>;
 }
 
-pub trait Listener<Ev: Copy> {
+pub trait Listener<Ev: Event> {
     // Return a view of of all events this listener
     // can be triggered by
     fn triggers(&self) -> &Vec<Ev>;
@@ -21,12 +30,14 @@ pub trait Listener<Ev: Copy> {
     // Handles dispatching individual triggers to specific
     // functionality.
     // May return any number of events in reaction.
-    fn dispatch(&self, trigger: Ev) -> Option<Vec<Ev>>;
+    fn dispatch(&self, trigger: &Ev) -> Option<Vec<Ev>>;
 }
 
 #[cfg(test)]
 mod tests {
-    use std::{any::Any};
+    use std::{any::Any, cell::RefCell, rc::Rc};
+    use crate::{EmCRc, LiCRc};
+
     use super::{event_handler::*, Emitter, Listener};
 
     #[derive(Debug, PartialEq, Copy, Clone)]
@@ -58,12 +69,12 @@ mod tests {
         fn on_triggers(&self, triggers: Vec<&TestEvents>) -> Option<Vec<TestEvents>> {
             let mut ret = vec![];
             for &t in triggers {
-                ret.extend(self.dispatch(t).unwrap());
+                ret.extend(self.dispatch(&t).unwrap());
             }
             Some(ret)
         }
 
-        fn dispatch(&self, trigger:TestEvents) -> Option<Vec<TestEvents>> {
+        fn dispatch(&self, trigger: &TestEvents) -> Option<Vec<TestEvents>> {
             match trigger {
                 _ => None
             }
@@ -77,10 +88,10 @@ mod tests {
     // *** Tests start here *** //
     #[test]
     fn empty_initialization() {
-        let eh: EventHandler<TestEvents> = EventHandler::new(None, None, None);
+        let eh: EventHandler<TestEvents> = EventHandler::new(vec![], vec![], vec![]);
         let e_v: Vec<TestEvents> = Vec::new();
-        let em_v: Vec<Box<dyn Emitter<TestEvents>>> = Vec::new();
-        let li_v: Vec<Box<dyn Listener<TestEvents>>> = Vec::new();
+        let em_v: Vec<EmCRc<TestEvents>> = Vec::new();
+        let li_v: Vec<LiCRc<TestEvents>> = Vec::new();
 
         assert_eq!(eh.get_stack().len(), 0);
         assert_eq!(eh.get_stack().type_id(), e_v.type_id());
@@ -94,7 +105,7 @@ mod tests {
 
     #[test]
     fn stack_manipulation() {
-        let mut eh: EventHandler<TestEvents> = EventHandler::new(None, None, None);
+        let mut eh: EventHandler<TestEvents> = EventHandler::new(vec![], vec![], vec![]);
         let ev: Vec<TestEvents> = Vec::new();
 
         eh.push_event(Some(TestEvents::E1));
@@ -122,19 +133,19 @@ mod tests {
 
     #[test]
     fn emitter_creation_and_addition() {
-        let mut eh: EventHandler<TestEvents> = EventHandler::new(None, None, None);
-        let e_v: Vec<Box<dyn Emitter<TestEvents>>> = Vec::new();
+        let mut eh: EventHandler<TestEvents> = EventHandler::new(vec![], vec![], vec![]);
+        let e_v: Vec<EmCRc<TestEvents>> = Vec::new();
         let em = TestEmitter { emissions: Some(vec![TestEvents::E4(3)]) };
 
         assert_eq!(eh.get_emitters().len(), 0);
         assert_eq!(eh.get_emitters().type_id(), e_v.type_id());
 
-        eh.register_emitter(Box::new(em));
+        eh.register_emitter(Rc::new(RefCell::new(em)));
 
         assert_eq!(eh.get_emitters().len(), 1);
-        assert_eq!(eh.get_emitters()[0].emit(), &Some(vec![TestEvents::E4(3)]));
+        assert_eq!(eh.get_emitters()[0].borrow().emit(), &Some(vec![TestEvents::E4(3)]));
 
-        let ems: Vec<Box<dyn Emitter<TestEvents>>> = vec![Box::new(TestEmitter {emissions: Some(vec![TestEvents::E2])})];
+        let ems: Vec<EmCRc<TestEvents>> = vec![Rc::new(RefCell::new(TestEmitter {emissions: Some(vec![TestEvents::E2])}))];
         eh.register_emitters(ems);
 
         // todo: Finish test
@@ -142,19 +153,19 @@ mod tests {
 
     #[test]
     fn listener_creation_and_addition() {
-        let mut eh: EventHandler<TestEvents> = EventHandler::new(None, None, None);
-        let e_v: Vec<Box<dyn Listener<TestEvents>>> = Vec::new();
+        let mut eh: EventHandler<TestEvents> = EventHandler::new(vec![], vec![], vec![]);
+        let e_v: Vec<LiCRc<TestEvents>> = Vec::new();
         let li = TestListener { triggers: vec![TestEvents::E2] };
 
         assert_eq!(eh.get_listeners().len(), 0);
         assert_eq!(eh.get_listeners().type_id(), e_v.type_id());
 
-        eh.register_listener(Box::new(li));
+        eh.register_listener(Rc::new(RefCell::new(li)));
 
         assert_eq!(eh.get_listeners().len(), 1);
-        assert!(eh.get_listeners()[0].triggers().contains(&TestEvents::E2));
+        assert!(eh.get_listeners()[0].borrow().triggers().contains(&TestEvents::E2));
 
-        let lis: Vec<Box<dyn Listener<TestEvents>>> = vec![Box::new(TestListener {triggers: vec![TestEvents::E3]})];
+        let lis: Vec<LiCRc<TestEvents>> = vec![Rc::new(RefCell::new(TestListener {triggers: vec![TestEvents::E3]}))];
         eh.register_listeners(lis);
 
         // todo: Finish test
