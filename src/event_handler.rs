b17709bcae3e563782ea7246ea0f158e2a1f8a02
ob2001 <butlero2001@outlook.com>
2025-09-19 15:53:25 -0400

    Fix functions. Add debug messages.

--- src/event_handler.rs
+++ src/event_handler.rs
@@ -18,22 +18,45 @@ impl<'a, Ev: Event<'a>> EventHandler<'a, Ev> {
     }
 
     pub fn register_emitter(&mut self, emitter: EmMutRef<'a, Ev>) {
+        #[cfg(debug_assertions)]
+        println!("Emitter registered");
+
         self.emitters.push(emitter);
     }
 
     pub fn register_listener(&mut self, listener: LiMutRef<'a, Ev>) {
+        #[cfg(debug_assertions)]
+        println!("Listener registered");
+
         self.listeners.push(listener);
     }
 
     pub fn register_emitters(&mut self, emitters: &mut Vec<EmMutRef<'a, Ev>>) {
+        while let Some(em) = emitters.pop() {
+            #[cfg(debug_assertions)]
+            println!("Emitter registered");
+
+            self.register_emitter(em);
+        }
     }
 
     pub fn register_listeners(&mut self, listeners: &mut Vec<LiMutRef<'a, Ev>>) {
+        while let Some(li) = listeners.pop() {
+            #[cfg(debug_assertions)]
+            println!("Listener registered");
+
+            self.register_listener(li);
+        }
     }
 
     pub fn push_event(&mut self, event: Option<Ev>) {
         match event {
-            Some(e) => self.stack.push(e),
+            Some(e) => {
+                #[cfg(debug_assertions)]
+                println!("Event pushed to stack: {:?}", e);
+
+                self.stack.push(e)
+            },
             _ => {}
         }
     }
@@ -42,6 +65,9 @@ impl<'a, Ev: Event<'a>> EventHandler<'a, Ev> {
         match events {
             None => {}
             Some(e) => {
+                #[cfg(debug_assertions)]
+                println!("Events pushed to stack: {:?}", e);
+
                 self.stack.extend(e);
             }
         }
@@ -60,11 +86,17 @@ impl<'a, Ev: Event<'a>> EventHandler<'a, Ev> {
     }
 
     pub fn peek_next_event(&self) -> Option<&Ev> {
+        #[cfg(debug_assertions)]
+        println!("Event peeked: {:?}", self.stack.first());
+
         self.stack.first()
     }
 
     pub fn pop_next_event(&mut self) -> Option<Ev> {
         let ret = self.stack.pop();
+        #[cfg(debug_assertions)]
+        println!("Event popped: {:?}", ret);
+
         self.prev_event = ret.clone();
         ret
     }
