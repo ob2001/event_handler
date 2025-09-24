c64e89dc29cb21f44d58276a3329a33ded77e5a1
ob2001 <butlero2001@outlook.com>
2025-09-23 20:27:59 -0400

    Add default "conversant" := both emitter and listener

--- src/conversant.rs
+++ src/conversant.rs
@@ -2,43 +2,50 @@ use std::{fmt::Debug, time::{SystemTime, UNIX_EPOCH}};
 use crate::{EHRc, Event, IEmitter, IListener};
 
 #[derive(Debug, Clone)]
-pub struct Conversant<Ev: Event> {
+pub struct DefConversant<Ev: Event> {
     parents: Vec<EHRc<Ev>>,
+    triggers: Vec<Ev>,
     id: usize
 }
 
-impl<Ev: Event> Conversant<Ev>  {
+impl<Ev: Event> DefConversant<Ev>  {
     pub fn new(parents: Vec<EHRc<Ev>>) -> Self {
-        Conversant { parents, id: SystemTime::now().duration_since(UNIX_EPOCH).expect("").as_millis() as usize }
+        DefConversant { parents, triggers: Vec::new(), id: SystemTime::now().duration_since(UNIX_EPOCH).expect("").as_millis() as usize }
     }
 }
 
-impl<Ev: Event> PartialEq for Conversant<Ev> {
+impl<Ev: Event> PartialEq for DefConversant<Ev> {
     fn eq(&self, other: &Self) -> bool {
         self.id == other.id
     }
 }
 
-impl<Ev: Event> IEmitter<Ev> for Conversant<Ev> {
-    fn add_parent(&mut self, parent: crate::EHRc<Ev>) {
-        todo!();
-    }
+impl<Ev: Event> IEmitter<Ev> for DefConversant<Ev> {
     fn emit(&self) -> Option<Vec<Ev>> {
-        todo!();
+        // todo
+        None
     }
-    fn get_parents(&self) -> Vec<crate::EHRc<Ev>> {
-        todo!();
+    fn add_parent(&mut self, parent: EHRc<Ev>) {
+        self.parents.push(parent);
+    }
+    fn get_parents(&self) -> Vec<EHRc<Ev>> {
+        self.parents.clone()
     }
 }
 
-impl<Ev: Event> IListener<Ev> for Conversant<Ev>  {
-    fn dispatch(&self, trigger: &Ev) -> Option<Vec<Ev>> {
-        todo!();
-    }
+impl<Ev: Event> IListener<Ev> for DefConversant<Ev>  {
     fn get_triggers(&self) -> Vec<&Ev> {
-        todo!();
+        let mut ret = vec![];
+        for t in &self.triggers {
+            ret.push(t);
+        }
+        ret
     }
-    fn on_triggers(&self, triggers: Vec<&Ev>) -> Option<Vec<Ev>> {
-        todo!();
+    fn on_triggers(&self, triggers: Vec<&Ev>) {
+        for t in triggers {
+            match t {
+                _ => {}
+            }
+        }
     }
 }
