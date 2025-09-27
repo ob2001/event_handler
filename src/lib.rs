c16cb066603fd7320836077896e936fdf32eb4cc
ob2001 <butlero2001@outlook.com>
2025-09-27 19:42:42 -0400

    Name shortening

--- src/lib.rs
+++ src/lib.rs
@@ -15,12 +15,11 @@ pub static IDCOUNTER: AtomicUsize = AtomicUsize::new(0);
 
 #[cfg(test)]
 mod tests {
-    use crate::conversant::DefConversant;
     use crate::prelude::*;
-    use crate::{event_handler::EventHandler, emitter::DefEmitter, listener::DefListener};
+    use crate::{event_handler::EventHandler as EH, emitter::DefEmitter as DEm, listener::DefListener as DLi, conversant::DefConversant as DCo};
 
     #[derive(Debug, PartialEq, Copy, Clone)]
-    enum TestEvents {
+    enum TEv {
         E1,
         E2,
         E3,
@@ -31,15 +30,15 @@ mod tests {
     // *** Tests start here *** //
     #[test]
     fn empty_initializations() {
-        use TestEvents::*;
-        let eh1 = EventHandler::<TestEvents, usize>::new_ehrc();
-        let eh2 = EventHandler::<TestEvents, usize>::new_ehrc();
-        let em1 = DefEmitter::new(vec![eh1.clone()]);
-        let em2 = DefEmitter::new(vec![]);
-        let li1 = DefListener::new(vec![E1, E2, E3, E4(3), E5("Hi")]);
-        let li2 = DefListener::new(vec![]);
-        let co1 = DefConversant::<TestEvents>::new(vec![eh2.clone()], None);
-        let co2 = DefConversant::<TestEvents>::new(vec![], Some(vec![E1, E5("Bye")]));
+        use TEv::*;
+        let eh1 = EH::<TEv, usize>::new_ehrc();
+        let eh2 = EH::<TEv, usize>::new_ehrc();
+        let em1 = DEm::new(vec![eh1.clone()]);
+        let em2 = DEm::new(vec![]);
+        let li1 = DLi::new(vec![E1, E2, E3, E4(3), E5("Hi")]);
+        let li2 = DLi::new(vec![]);
+        let co1 = DCo::<TEv>::new(vec![eh2.clone()], None);
+        let co2 = DCo::<TEv>::new(vec![], Some(vec![E1, E5("Bye")]));
 
         assert_ne!(eh1, eh2);
         assert_ne!(em1, em2);
@@ -52,7 +51,7 @@ mod tests {
         assert_eq!(em1.get_handlers()[0], eh1);
 
         assert_eq!(co1.get_handlers()[0], eh2);
-        
+
         println!("{:?}", eh1.borrow());
         println!("{:?}", eh2.borrow());
         println!("{:?}", em1);
@@ -65,27 +64,27 @@ mod tests {
 
     #[test]
     fn stack_manipulation() {
-        let eh = EventHandler::<TestEvents, usize>::new_ehrc();
-        let em = DefEmitter::new_emrc(vec![eh.clone()]);
+        let eh = EH::<TEv, usize>::new_ehrc();
+        let em = DEm::new_emrc(vec![eh.clone()]);
 
         assert_eq!(eh.borrow().get_stack().len(), 0);
 
-        eh.borrow_mut().push_event(Some((em.clone(), TestEvents::E1)));
+        eh.borrow_mut().push_event(Some((em.clone(), TEv::E1)));
 
         assert_eq!(eh.borrow().get_stack().len(), 1);
 
         assert_eq!(eh.borrow().peek_next_emitter().unwrap().borrow().get_id(), em.borrow().get_id());
-        assert_eq!(eh.borrow().peek_next_event().unwrap(), &TestEvents::E1);
+        assert_eq!(eh.borrow().peek_next_event().unwrap(), &TEv::E1);
     }
 
     #[test]
     fn emitter_creation_and_addition() {
-        // todo: Finish test
+        // todo
     }
 
     #[test]
     fn listener_creation_and_addition() {
-        // todo: Finish Test
+        // todo
     }
 
     #[test]
