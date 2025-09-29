1d780d1f117c7a940c8c46b8860f2983e222a7fa
ob2001 <butlero2001@outlook.com>
2025-09-28 21:05:09 -0400

    Fix some names, start implementing emitter tests

--- src/lib.rs
+++ src/lib.rs
@@ -24,27 +24,27 @@ mod tests {
     };
 
     #[derive(Debug, PartialEq, Copy, Clone)]
-    enum TestEvents {
-        E1,
-        E2,
-        E3,
-        E4(i32),
-        E5(&'static str),
+    enum TestTags {
+        T1,
+        T2,
+        T3,
+        T4(i32),
+        T5(&'static str),
     }
 
     // *** Tests start here *** //
     #[test]
     fn empty_initializations() {
-        use TestEvents::{self as TEv, *};
+        use TestTags::{self, *};
 
-        let eh1 = EH::<TEv, usize>::new_ehrc();
-        let eh2 = EH::<TEv, usize>::new_ehrc();
-        let em1 = DEm::new(vec![eh1.clone()]);
-        let em2 = DEm::new(vec![]);
-        let li1 = DLi::new(vec![E1, E2, E3, E4(3), E5("Hi")]);
-        let li2 = DLi::new(vec![]);
-        let emli1 = DEmLi::<TEv>::new(vec![eh2.clone()], None);
-        let emli2 = DEmLi::<TEv>::new(vec![], Some(vec![E1, E5("Bye")]));
+        let eh1 = EH::<TestTags, usize>::new_ehrc();
+        let eh2 = EH::<TestTags, usize>::new_ehrc();
+        let em1 = DEm::new_emrc(vec![eh1.clone()], None);
+        let em2 = DEm::new_emrc(vec![], None);
+        let li1 = DLi::new_lirc(vec![T1, T2, T3, T4(3), T5("Hi")]);
+        let li2 = DLi::new_lirc(vec![]);
+        let emli1 = DEmLi::<TestTags>::new(vec![eh2.clone()], None, None);
+        let emli2 = DEmLi::<TestTags>::new(vec![], Some(vec![T1, T5("Bye")]), None);
 
         assert_ne!(eh1, eh2);
         assert_ne!(em1, em2);
@@ -54,11 +54,12 @@ mod tests {
         assert_eq!(eh1.borrow().get_stack_len(), 0);
         assert_eq!(eh1.borrow().get_listeners().len(), 0);
 
-        assert_eq!(em1.get_handlers()[0], eh1);
-        assert_eq!(emli1.get_handlers()[0], eh2);
+        assert!(em1.borrow().has_handler(&eh1));
+        assert!(emli1.has_handler(&eh2));
+        assert!(emli2.has_trigger(&T1));
 
-        println!("{:?}", eh1.borrow());
-        println!("{:?}", eh2.borrow());
+        println!("{:?}", eh1);
+        println!("{:?}", eh2);
         println!("{:?}", em1);
         println!("{:?}", em2);
         println!("{:?}", li1);
@@ -69,43 +70,54 @@ mod tests {
 
     #[test]
     fn stack_manipulation() {
-        use TestEvents::{self as TEv, *};
+        use TestTags::{self, *};
         
-        let eh = EH::<TEv, usize>::new_ehrc();
-        let em = DEm::new_emrc(vec![eh.clone()]);
+        let eh = EH::<TestTags, usize>::new_ehrc();
+        let em = DEm::new_emrc(vec![eh.clone()], None);
 
         println!("{:?}", eh.borrow());
         assert_eq!(eh.borrow().get_stack_len(), 0);
 
-        eh.borrow_mut().push_event(Some(Event::new(em.clone(), Some(E1))));
+        eh.borrow_mut().push_event(Some(Event::new(em.clone(), Some(T1))));
         println!("{:?}", eh.borrow());
 
         assert_eq!(eh.borrow().get_stack_len(), 1);
         assert_eq!(eh.borrow().peek_next_emitter().unwrap().borrow().get_id(), em.borrow().get_id());
-        assert_eq!(eh.borrow().peek_next_tag().unwrap(), E1);
+        assert_eq!(eh.borrow().peek_next_tag().unwrap(), T1);
 
-        eh.borrow_mut().push_events(Some(vec![Event::new(em.clone(), Some(E3)), Event::new(em.clone(), Some(E5("A")))]));
+        eh.borrow_mut().push_events(Some(vec![Event::new(em.clone(), Some(T3)), Event::new(em.clone(), Some(T5("A")))]));
         println!("{:?}", eh.borrow());
 
         assert_eq!(eh.borrow().get_stack_len(), 3);
         assert_eq!(eh.borrow().peek_next_emitter().unwrap().borrow().get_id(), em.borrow().get_id());
-        assert_eq!(eh.borrow().peek_next_tag(), Some(E5("A")));
+        assert_eq!(eh.borrow().peek_next_tag(), Some(T5("A")));
 
         let next = eh.borrow_mut().pop_next();
         println!("{:?}", eh.borrow());
 
         assert_eq!(next.as_ref().unwrap().get_emitter().borrow().get_id(), em.borrow().get_id());
-        assert_eq!(next.as_ref().unwrap().get_tag(), Some(E5("A")));
+        assert_eq!(next.as_ref().unwrap().get_tag(), Some(T5("A")));
 
         assert_eq!(eh.borrow().get_stack_len(), 2);
         assert_eq!(eh.borrow().peek_next_emitter().unwrap().borrow().get_id(), em.borrow().get_id());
-        assert_eq!(eh.borrow().peek_next_tag(), Some(E3));
+        assert_eq!(eh.borrow().peek_next_tag(), Some(T3));
         assert_eq!(eh.borrow().get_prev_event(), next.as_ref());
     }
 
     #[test]
     fn emitter_creation_and_addition() {
-        // todo
+        use crate::event_handler::register_emitter;
+        use TestTags::{self, *};
+        let eh = EH::<TestTags, usize>::new_ehrc();
+        let em = DEm::<TestTags>::new_emrc(vec![], Some(T1));
+
+        println!("{:?}", eh.borrow());
+        println!("{:?}", em.borrow());
+
+        register_emitter(&eh, &(em.clone() as EmRC<TestTags, usize>));
+
+        println!("{:?}", eh.borrow());
+        println!("{:?}", em.borrow());
     }
 
     #[test]
